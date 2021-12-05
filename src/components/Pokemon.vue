15f962be02bf5b8d91ce4b94705f47b6ff897ce8
octaviohmb <octaviohmb@gmail.com>
2021-12-05 12:46:30 -0300

    base pokemon card

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card>
+    <v-card-title style="font-size:2em">#35 - Clefary</v-card-title>
+    <v-row justify="center">
+      <v-col cols="5">
+        <v-row justify="center">
+          <v-img
+            class="my-6"
+            max-width="200px"
+            contain
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/35.png"
+          ></v-img>
+        </v-row>
+
+        <div class="text-center">
+          <v-chip
+            class="ma-2"
+            color="pink"
+          >
+            FAIRY
+          </v-chip>
+        </div>
+      </v-col>
+
+      <v-col cols="7"></v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+// export default {
+//   props: ['pokemon']
+// }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,15 @@
 <template>
-  <div></div>
+  <v-container>
+    <Pokemon />
+  </v-container>
 </template>
+
+<script>
+import Pokemon from '@/components/Pokemon'
+
+export default {
+  components: {
+    Pokemon
+  }
+}
+</script>
